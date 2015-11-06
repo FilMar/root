40ebd4942834e82d05e741360550da24955b0ed7
FilMar <pippo.home32@gmail.com>
2015-11-06 18:48:40 +0100

    root.c
    this is my algorithm for calculate any root

---
+++ root.c
@@ -0,0 +1,17 @@
+double pradice(double b,double r){
+        double e,c,d, base;
+        c=1;
+	while (2<3){
+	  base = r;
+          for(e=1;base>0;e*=c,base--);
+                      
+          d=c;
+          c=c+0.0001;	
+          
+          
+          
+          if (e>=b || c >= b) break;
+
+          }
+       return d;
+}
